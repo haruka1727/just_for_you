cceb1eda73be454164d24d84a08862583e98290a
haruka1727 <giovydimauro@live.it>
2024-12-15 21:11:07 +0100

    New phrases

--- src/components/FirstPage.vue
+++ src/components/FirstPage.vue
@@ -20,11 +20,47 @@ export default {
   setup() {
     // Stato reattivo
     const phrases = [
-        "Life is what happens when you're busy making other plans.",
-        "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-        "The way to get started is to quit talking and begin doing.",
-        "Don't let yesterday take up too much of today.",
-        "You only live once, but if you do it right, once is enough."
+        "Doubt thou the stars are fire, Doubt that the sun doth move, Doubt truth to be a liar, But never doubt I love",
+        "Hear my soul speak. Of the very instant that I saw you, Did my heart fly at your service",
+        "You are the bane of my existence and the object of all my desires.",
+        "I Burn for You",
+        "I love all of you. Even the parts that you believe are too dark and too shameful. Every scar. Every flaw. Every imperfection. I love you",
+        "I want a life that suits us both. I know I am imperfect, but I will humble myself before you because I cannot imagine my life without you",
+        "I cannot stop thinking of you. From the mornings you ease to the evenings you quiet, to the dreams you inhabit... My thoughts of you never end. I am yours. I have always been yours.",
+        "And in fact, preferring sleep because that is where I might find you",
+        "You occupy my every thought.",
+        "I know you will lift my spirits and make me see the world in ways I could not have imagined.",
+        "True love is something else entirely. It is when the rest of the world goes quiet. It is not eyes that meet, but souls that dance. Settle into each other. Make room for each other. Until there is nowhere else to hide.",
+        "I have not been able to sleep, not been able to eat. I can barely speak these days. My entire thoughts consumed by. By our kiss. By you.",
+        "I don't need to understand the plan! I trust my love.",
+        "I saw that you were perfect, and so I loved you. Then I saw that you were not perfect and I loved you even more",
+        "To the world you may be one person, but to one person you are the world",
+        "I need you like a heart needs a beat.",
+        "If I had a flower for every time I thought of you...I could walk through my garden forever",
+        "I love you for all that you are, all that you have been and all that you will be",
+        "You are the best thing that's ever been mine",
+        "You make me want to be a better woman.",
+        "I never want to stop making memories with you.",
+        "Your hand touching mine. This is how galaxies collide.",
+        "To be your friend was all I ever wanted; to be your lover was all I ever dreamed.",
+        "Take my hand, take my whole life too. For I can't help falling in love with you.",
+        "And in his smile I see something more beautiful than the stars.",
+        "Because of you, I can feel myself slowly, but surely, becoming the me I have always dreamed of being.",
+        "Our love is like the wind. I can't see it, but I can feel it",
+        "I love that you are the last person I want to talk to before I go to sleep at night.",
+        "He's more myself than I am. Whatever our souls are made of, his and mine are the same.",
+        "I want all of you, forever, you and me, every day.",
+        "I can't see anything I don't like about you.",
+        "To me, you are perfect.",
+        "You had me at hello.",
+        "I love you. You complete me",
+        "Kiss me. Kiss me as if it were the last time",
+        "I like you very much. Just as you are.",
+        "You want the moon? Just say the word, and I'll throw a lasso around it and pull it down",
+        "I think I'd miss you even if we'd never met",
+
+
+
     ];
     const randomPhrase = ref("");
 
